7aee6a9e5779560e07064801b0ec041d7ecbd6e5
Khang Nguyen <khangnguyen@Khangs-MacBook-Pro-7.local>
2024-11-05 17:11:07 -0500

    New map and Enemywave

---
+++ src/Collectibles/HP.java
@@ -0,0 +1,58 @@
+package Collectibles;
+
+import Builders.FrameBuilder;
+import Engine.GraphicsHandler;
+import Engine.ImageLoader;
+import GameObject.Frame;
+import GameObject.SpriteSheet;
+import Level.Map;
+import Level.NPC;
+import Level.Player;
+import Utils.Point;
+import java.util.HashMap;
+
+//class that handles coincollection and the likes
+public class HP extends NPC {
+
+    protected Map mapPosition;
+
+    public HP(Point location, Map map) {
+        super(location.x, location.y, new SpriteSheet(ImageLoader.load("HP.png"), 24, 24), "DEFAULT");
+        isInteractable = true;
+        this.mapPosition = map;
+    }
+
+    // for player intersection
+    public void update(Player player) {
+        super.update();
+
+        if (intersects(player)) {
+             collectpowerup(player);
+        }
+    }
+
+    private void collectpowerup(Player player) {
+        player.increaseHealth();
+        mapPosition.getNPCs().remove(this);
+
+    }
+
+    @Override
+    public void draw(GraphicsHandler graphicsHandler) {
+        super.draw(graphicsHandler);
+    }
+
+    @Override
+    public HashMap<String, Frame[]> loadAnimations(SpriteSheet spriteSheet) {
+        return new HashMap<String, Frame[]>() {
+            {
+                put("DEFAULT", new Frame[] {
+                        new FrameBuilder(spriteSheet.getSprite(0, 0))
+                                .withScale(2)
+                                .withBounds(1, 1, 38, 38)
+                                .build()
+                });
+            }
+        };
+    }
+}
