8f0da00b2d500841f15e647658bd730f55c5b5d7
Jace Pivarnik <jacepivarnik@Jaces-MacBook-Pro.local>
2024-11-07 15:50:20 -0500

    Fix map

--- src/Level/Map.java
+++ src/Level/Map.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Scanner;
 
 
-import NPCs.WeaponPickup;
+import NPCs.APistolPickup;
 import java.util.Random;
 
 
--- src/Maps/Map1.java
+++ src/Maps/Map1.java
@@ -9,10 +9,14 @@ import EnhancedMapTiles.HorizontalMovingPlatform;
 import GameObject.Rectangle;
 import Level.*;
 import NPCs.Walrus;
-import NPCs.WeaponPickup;
+import NPCs.APistolPickup;
+import NPCs.AAsaultRiflePickup;
+import NPCs.AShotgunPickup;
 import Tilesets.CommonTileset;
 import Utils.Direction;
 import java.util.ArrayList;
+
+import Collectibles.Coin;
 
 
 // Represents a test map to be used in a level
@@ -24,7 +28,7 @@ public class Map1 extends Map {
        this.playerStartPosition = getMapTile(2, 11).getLocation();
    }
 
-
+   /* 
    @Override
    public ArrayList<Enemy> loadEnemies() {
        ArrayList<Enemy> enemies = new ArrayList<>();
@@ -40,7 +44,7 @@ public class Map1 extends Map {
 
        return enemies;
    }
-
+    */
 
    @Override
    public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
@@ -70,15 +74,31 @@ public class Map1 extends Map {
    @Override
    public ArrayList<NPC> loadNPCs() {
        ArrayList<NPC> npcs = new ArrayList<>();
-  
+
+
        // Adding the Walrus NPC
        Walrus walrus = new Walrus(getMapTile(30, 10).getLocation().subtractY(13));
        npcs.add(walrus);
-  
-       // Adding the WeaponPickup NPC
-       WeaponPickup weaponPickup = new WeaponPickup(getMapTile(7, 15).getLocation(), this);
-       npcs.add(weaponPickup);
-  
+
+
+       APistolPickup apistolPickup = new APistolPickup(getMapTile(9, (int)8).getLocation(), this);
+      npcs.add(apistolPickup);
+
+
+
+
+      AAsaultRiflePickup aasaultriflePickup = new AAsaultRiflePickup(getMapTile(16, (int)6).getLocation(), this);
+      npcs.add(aasaultriflePickup);
+
+
+
+
+      AShotgunPickup ashotgunPickup = new AShotgunPickup(getMapTile(12, (int)11).getLocation(), this);
+      npcs.add(ashotgunPickup);
+
+
+       Coin coin = new Coin(getMapTile(10, 11).getLocation(), this);
+       npcs.add(coin);
        return npcs;
    }
     
