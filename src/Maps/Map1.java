080552d9f98c25e50a09da9378b66f715b12ed7d
Khang Nguyen <khangnguyen@Khangs-MacBook-Pro-8.local>
2024-11-07 14:27:54 -0500

    Enemy wave implemented

--- src/Maps/Map1.java
+++ src/Maps/Map1.java
@@ -1,5 +1,6 @@
 package Maps;
 
+
 import Enemies.BugEnemy;
 import Enemies.DinosaurEnemy;
 import Engine.ImageLoader;
@@ -7,75 +8,87 @@ import EnhancedMapTiles.EndLevelBox;
 import EnhancedMapTiles.HorizontalMovingPlatform;
 import GameObject.Rectangle;
 import Level.*;
-import NPCs.AAsaultRiflePickup;
-import NPCs.APistolPickup;
-import NPCs.AShotgunPickup;
 import NPCs.Walrus;
+import NPCs.WeaponPickup;
 import Tilesets.CommonTileset;
 import Utils.Direction;
 import java.util.ArrayList;
 
+
 // Represents a test map to be used in a level
 public class Map1 extends Map {
 
-    public Map1() {
-        super("map1.txt", new CommonTileset());
-        this.playerStartPosition = getMapTile(32, 7).getLocation();
-    }
 
-    @Override
-    public ArrayList<Enemy> loadEnemies() {
-        ArrayList<Enemy> enemies = new ArrayList<>();
-        /* 
-        BugEnemy bugEnemy = new BugEnemy(getMapTile(16, 10).getLocation().subtractY(25), Direction.LEFT);
-        enemies.add(bugEnemy);
+   public Map1() {
+       super("map1.txt", new CommonTileset());
+       this.playerStartPosition = getMapTile(2, 11).getLocation();
+   }
 
-        DinosaurEnemy dinosaurEnemy = new DinosaurEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT);
-        enemies.add(dinosaurEnemy);
-        */
-        return enemies;
-    }
 
-    @Override
-    public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
-        ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+   @Override
+   public ArrayList<Enemy> loadEnemies() {
+       ArrayList<Enemy> enemies = new ArrayList<>();
 
-        HorizontalMovingPlatform hmp = new HorizontalMovingPlatform(
-                ImageLoader.load("GreenPlatform.png"),
-                getMapTile(24, 6).getLocation(),
-                getMapTile(27, 6).getLocation(),
-                TileType.JUMP_THROUGH_PLATFORM,
-                3,
-                new Rectangle(0, 6,16,4),
-                Direction.RIGHT
-        );
-        enhancedMapTiles.add(hmp);
 
-        EndLevelBox endLevelBox = new EndLevelBox(getMapTile(32, 7).getLocation());
-        enhancedMapTiles.add(endLevelBox);
+       BugEnemy bugEnemy = new BugEnemy(getMapTile(16, 10).getLocation().subtractY(25), Direction.LEFT);
+       enemies.add(bugEnemy);
 
-        return enhancedMapTiles;
-    }
 
-    @Override
-    public ArrayList<NPC> loadNPCs() {
-        ArrayList<NPC> npcs = new ArrayList<>();
+       DinosaurEnemy dinosaurEnemy = new DinosaurEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT);
+       enemies.add(dinosaurEnemy);
+
+
+       return enemies;
+   }
+
+
+   @Override
+   public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+       ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+
+
+       HorizontalMovingPlatform hmp = new HorizontalMovingPlatform(
+               ImageLoader.load("GreenPlatform.png"),
+               getMapTile(24, 6).getLocation(),
+               getMapTile(27, 6).getLocation(),
+               TileType.JUMP_THROUGH_PLATFORM,
+               3,
+               new Rectangle(0, 6,16,4),
+               Direction.RIGHT
+       );
+       enhancedMapTiles.add(hmp);
+
+
+       EndLevelBox endLevelBox = new EndLevelBox(getMapTile(32, 7).getLocation());
+       enhancedMapTiles.add(endLevelBox);
+
+
+       return enhancedMapTiles;
+   }
+
+
+   @Override
+   public ArrayList<NPC> loadNPCs() {
+       ArrayList<NPC> npcs = new ArrayList<>();
+  
+       // Adding the Walrus NPC
+       Walrus walrus = new Walrus(getMapTile(30, 10).getLocation().subtractY(13));
+       npcs.add(walrus);
+  
+       // Adding the WeaponPickup NPC
+       WeaponPickup weaponPickup = new WeaponPickup(getMapTile(7, 15).getLocation(), this);
+       npcs.add(weaponPickup);
+  
+       return npcs;
+   }
     
-        // Adding the Walrus NPC
-        Walrus walrus = new Walrus(getMapTile(30, 10).getLocation().subtractY(13));
-        npcs.add(walrus);
-    
-        APistolPickup apistolPickup = new APistolPickup(getMapTile(9, (int)8).getLocation(), this);
-       npcs.add(apistolPickup);
 
 
-       AAsaultRiflePickup aasaultriflePickup = new AAsaultRiflePickup(getMapTile(16, (int)6).getLocation(), this);
-       npcs.add(aasaultriflePickup);
 
 
-       AShotgunPickup ashotgunPickup = new AShotgunPickup(getMapTile(12, (int)11).getLocation(), this);
-       npcs.add(ashotgunPickup); 
-    
-        return npcs;
-    }
+   @Override
+   protected ArrayList<ArrayList<Enemy>> loadEnemyWaves() {
+       // TODO
+       throw new UnsupportedOperationException("Unimplemented method 'loadEnemyWaves'");
+   }
 }
--- src/Maps/TestMap.java
+++ src/Maps/TestMap.java
@@ -1,5 +1,6 @@
 package Maps;
 
+
 import Collectibles.Coin;
 import Enemies.ZombieEnemy;
 import Engine.ImageLoader;
@@ -16,56 +17,97 @@ import Utils.Direction;
 import Utils.Point;
 import java.util.ArrayList;
 
+
 // Represents a test map to be used in a level
 public class TestMap extends Map {
 
-    public TestMap() {
-        super("test_map.txt", new CommonTileset());
-        this.playerStartPosition = getMapTile(2, 11).getLocation();
-    }
 
-    @Override
-    public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
-        ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
-
-        HorizontalMovingPlatform hmp = new HorizontalMovingPlatform(
-                ImageLoader.load("GreenPlatform.png"),
-                getMapTile(24, 6).getLocation(),
-                getMapTile(27, 6).getLocation(),
-                TileType.JUMP_THROUGH_PLATFORM,
-                3,
-                new Rectangle(0, 6, 16, 4),
-                Direction.RIGHT);
-        // enhancedMapTiles.add(hmp);
-
-        EndLevelBox endLevelBox = new EndLevelBox(getMapTile(32, 7).getLocation());
-        // enhancedMapTiles.add(endLevelBox);
-
-        return enhancedMapTiles;
-    }
-
-    @Override
-    public ArrayList<NPC> loadNPCs() {
-        ArrayList<NPC> npcs = new ArrayList<>();
-
-        // Adding the Walrus NPC
-        Walrus walrus = new Walrus(getMapTile(30, 10).getLocation().subtractY(13));
-        npcs.add(walrus);
-
-        APistolPickup apistolPickup = new APistolPickup(getMapTile(9, (int)8).getLocation(), this);
-       npcs.add(apistolPickup);
+   public TestMap() {
+       super("test_map.txt", new CommonTileset());
+       this.playerStartPosition = getMapTile(2, 11).getLocation();
+   }
 
 
-       AAsaultRiflePickup aasaultriflePickup = new AAsaultRiflePickup(getMapTile(16, (int)6).getLocation(), this);
-       npcs.add(aasaultriflePickup);
+   @Override
+   public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+       ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
 
 
-       AShotgunPickup ashotgunPickup = new AShotgunPickup(getMapTile(12, (int)11).getLocation(), this);
-       npcs.add(ashotgunPickup);
+       HorizontalMovingPlatform hmp = new HorizontalMovingPlatform(
+               ImageLoader.load("GreenPlatform.png"),
+               getMapTile(24, 6).getLocation(),
+               getMapTile(27, 6).getLocation(),
+               TileType.JUMP_THROUGH_PLATFORM,
+               3,
+               new Rectangle(0, 6, 16, 4),
+               Direction.RIGHT);
+       // enhancedMapTiles.add(hmp);
 
-        Coin coin = new Coin(getMapTile(10, 11).getLocation(), this);
-        npcs.add(coin);
-        return npcs;
-    }
 
+       EndLevelBox endLevelBox = new EndLevelBox(getMapTile(32, 7).getLocation());
+       // enhancedMapTiles.add(endLevelBox);
+
+
+       return enhancedMapTiles;
+   }
+
+
+   @Override
+   public ArrayList<NPC> loadNPCs() {
+       ArrayList<NPC> npcs = new ArrayList<>();
+
+
+       // Adding the Walrus NPC
+       Walrus walrus = new Walrus(getMapTile(30, 10).getLocation().subtractY(13));
+       npcs.add(walrus);
+
+
+       APistolPickup apistolPickup = new APistolPickup(getMapTile(9, (int)8).getLocation(), this);
+      npcs.add(apistolPickup);
+
+
+
+
+      AAsaultRiflePickup aasaultriflePickup = new AAsaultRiflePickup(getMapTile(16, (int)6).getLocation(), this);
+      npcs.add(aasaultriflePickup);
+
+
+
+
+      AShotgunPickup ashotgunPickup = new AShotgunPickup(getMapTile(12, (int)11).getLocation(), this);
+      npcs.add(ashotgunPickup);
+
+
+       Coin coin = new Coin(getMapTile(10, 11).getLocation(), this);
+       npcs.add(coin);
+       return npcs;
+   }
+
+
+   protected ArrayList<ArrayList<Enemy>> loadEnemyWaves() {
+       ArrayList<ArrayList<Enemy>> waves = new ArrayList<>();
+
+
+       // Define Wave 1: Add a few ZombieEnemies
+       ArrayList<Enemy> wave1 = new ArrayList<>();
+       wave1.add(new ZombieEnemy(new Point(250, 523), new Point(350, 523), Direction.RIGHT));
+       wave1.add(new ZombieEnemy(new Point(300, 523), new Point(400, 523), Direction.LEFT));
+       waves.add(wave1);
+
+
+       // Define Wave 2: Add more ZombieEnemies
+       ArrayList<Enemy> wave2 = new ArrayList<>();
+       wave2.add(new ZombieEnemy(new Point(100, 523), new Point(200, 523), Direction.RIGHT));
+       waves.add(wave2);
+
+
+       // Define Wave 3: Add even more ZombieEnemies
+       ArrayList<Enemy> wave3 = new ArrayList<>();
+       wave3.add(new ZombieEnemy(new Point(200, 523), new Point(400, 523), Direction.RIGHT));
+       wave3.add(new ZombieEnemy(new Point(250, 523), new Point(350, 523), Direction.LEFT));
+       waves.add(wave3);
+
+
+       return waves;
+   }
 }
