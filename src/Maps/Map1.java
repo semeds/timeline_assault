687f72e6448d5f3f90c848af84f1da2eb35cac36
Khang Nguyen <khangnguyen@Khangs-MacBook-Pro-75.local>
2024-11-17 15:19:36 -0500

    Map1 Flattened

--- src/Maps/Map1.java
+++ src/Maps/Map1.java
@@ -53,19 +53,6 @@ public class Map1 extends Map {
    public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
        ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
 
-
-       HorizontalMovingPlatform hmp = new HorizontalMovingPlatform(
-               ImageLoader.load("GreenPlatform.png"),
-               getMapTile(24, 6).getLocation(),
-               getMapTile(27, 6).getLocation(),
-               TileType.JUMP_THROUGH_PLATFORM,
-               3,
-               new Rectangle(0, 6,16,4),
-               Direction.RIGHT
-       );
-       enhancedMapTiles.add(hmp);
-
-
        EndLevelBox endLevelBox = new EndLevelBox(getMapTile(32, 7).getLocation());
        enhancedMapTiles.add(endLevelBox);
 
@@ -77,11 +64,6 @@ public class Map1 extends Map {
    @Override
    public ArrayList<NPC> loadNPCs() {
        ArrayList<NPC> npcs = new ArrayList<>();
-
-
-       // Adding the Walrus NPC
-       Walrus walrus = new Walrus(getMapTile(30, 10).getLocation().subtractY(13));
-       npcs.add(walrus);
 
        MPistolPickup mpistolPickup = new MPistolPickup(getMapTile(12, (int)8).getLocation(), this);
        npcs.add(mpistolPickup);
